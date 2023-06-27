b8defe45ed9a5144d6d9482d31035a7ec16bc955
matias sette <gimemaydana@gmail.com>
2023-06-27 01:30:41 -0300

    subiendo tp parte1-6

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3030;
+app.use(express.static('public'))
+app.listen(PORT, ()=> console.log('servidor corriendo en http://localhost:${PORT}'));
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
+app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
+app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
+
+
+
+
+
