9c213f0063520f653b7500dfb49062d3ff98f396
matias sette <gimemaydana@gmail.com>
2023-07-07 08:51:30 -0300

    segunda entrega del tp 2-6

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 app.use(express.static('public'))
-app.listen(PORT, ()=> console.log('servidor corriendo en http://localhost:${PORT}'));
+//app.listen(PORT, ()=> console.log('servidor corriendo en http://localhost:${PORT}'));
+app.listen(PORT,()=>console.log("servidor corriendo en http://localhost:" +  PORT));
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
 app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
 app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
