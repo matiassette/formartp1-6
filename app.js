ac09b2e506270fccaa87ecd6c6e61c02b41177d1
matiassette <gimemaydana@gmail.com>
2023-07-21 07:46:48 -0300

    subiendo entrega 4 de 6

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = 3031;
+const express = require('express')
+const path = require('path')
+const app = express()
 app.use(express.static('public'))
-//app.listen(PORT, ()=> console.log('servidor corriendo en http://localhost:${PORT}'));
-app.listen(PORT,()=>console.log("servidor corriendo en http://localhost:" +  PORT));
-app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
-app.get('/home',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
-app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'views' ,'home.html')))
+const PORT = 3030;
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT} `))
 
 
+//rutas
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
 
-
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+})
